0b87034a66a431ec96b028560c4d6df9cde91821
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-07 22:24:34 +0000

    updated binding Events.

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import EventBinding from "./components/EventBinding";
+
+function App() {
+  return <div className="App">
+      <EventBinding />
+  </div>;
+}
+
+export default App;
